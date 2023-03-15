777c9ca2f2340c74b0ae3104ba44ad7640715d59
shieldadchr <127294977+shieldadchr@users.noreply.github.com>
2023-03-15 17:19:10 +0700

    commit akhir

--- modul4_1302210044/Program.cs
+++ modul4_1302210044/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace modul4_1302210044
+{ 
+    class program
+    {
+        static void Main(String[] args)
+        {
+            kodeBuah buah = new kodeBuah();
+            Console.WriteLine("Kode Buah " + kodeBuah.buah.Anggur + " kodenya adalah: " + buah.getKodeBuah(kodeBuah.buah.Anggur));
+        }
+
+    }
+}
---
+++ modul4_1302210044/kodeBuah.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace modul4_1302210044
+{
+    public class kodeBuah
+    {
+        public enum buah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        }
+        public String getKodeBuah(buah B)
+        {
+            String[] arr = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+            return arr[(int)B];
+
+        }
+    }
+}
